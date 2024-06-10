6de1aea5610a02b625b090a7778098a3031107cd
mohamadkassem <mohamadkassem244@gmail.com>
2024-06-10 18:08:29 +0300

    add leftbar and videos content

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $(".header__left__bars").click(function(){
+    $(".header__left__bars , .content__leftside__mobile__bars , .blackscreen").click(function() {
         $(".content__leftside").toggleClass("open");
+        $(".content__leftside__sections").toggleClass("open");
+        $(".content__leftside__sections__item").toggleClass("open");
     });
-    
+
+    $(".header__mid__opensearch").click(function() {
+        toggleSearch(true);
+    });
+
+    $(".header__mid__closesearch").click(function() {
+        toggleSearch(false);
+    });
+
+    function toggleSearch(open) {
+        $(".header__left").toggleClass("close", open);
+        $(".header__right").toggleClass("close", open);
+        $(".header__mid__opensearch").toggleClass("close", open);
+        $(".header__mid__closesearch").toggleClass("open", open);
+        $(".header__mid__search").toggleClass("open", open);
+    }
+
 });
-
-
-
